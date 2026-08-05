1303fe05da7544de17523944bfce6184dedc2a7c
Wilson B. <kalmod@live.com>
2026-08-04 21:32:06 -0400

    Add: longest-repeating-substring-with-replacement - submission-10

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-10.go
@@ -0,0 +1,28 @@
+func characterReplacement(s string, k int) int {
+	// most frequent char in substring - length of substring
+
+	m := make(map[rune]int)
+	l, ans := 0, 0
+	max_f := 0
+	for idx, r := range s {
+		m[r] += 1
+		if m[r] > max_f {
+			max_f = m[r]
+		}
+		for (idx-l+1)-max_f > k {
+			m[rune(s[l])] -= 1
+			l += 1
+			for _, count := range m {
+				if count > max_f {
+					max_f = count
+				}
+			}
+		} 
+
+		if (idx-l+1) > ans {
+			ans = (idx-l+1)
+		}
+		
+	}
+	return ans
+}
