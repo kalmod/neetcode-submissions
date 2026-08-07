679886d3910fdeaed3d6b8544c80d0eddeb5ad53
Wilson B. <kalmod@live.com>
2026-08-06 21:05:26 -0400

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+	def minEatingSpeed(self, piles: List[int], h: int) -> int:
+		low = 1
+		high = max(piles)
+		ans = high
+		while low <= high:
+			k = low + (high-low)//2
+			hOFk = sum((p + k -1) // k for p in piles)
+			if hOFk > h: # too many hours to eat, need to be bigger
+				low = k + 1
+			else: # can eat within limit, move it down to find min
+				high = k-1	
+				ans = min(ans, k)
+		return ans
